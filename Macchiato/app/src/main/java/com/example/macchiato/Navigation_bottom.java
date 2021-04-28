2e3b67508dba5490fd717a54c3c316bfb97473dd
alejandroxp08 <alejandroxp08@gmail.com>
2021-04-28 15:15:56 -0400

    Navegacion inferior con titulos

--- Macchiato/app/src/main/java/com/example/macchiato/Navigation_bottom.java
+++ Macchiato/app/src/main/java/com/example/macchiato/Navigation_bottom.java
@@ -20,22 +20,22 @@ public class Navigation_bottom extends AppCompatActivity {
 
         showSelectedFragment(new HorarioFragment());
 
-        mBottomNavigation =(BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        mBottomNavigation = findViewById(R.id.bottomNavigationView);
 
-        mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId()== R.id.nav_perfil){
-                    showSelectedFragment(new PerfilFragment());
-                }
-                if(item.getItemId()==R.id.nav_horario){
-                    showSelectedFragment(new HorarioFragment());
-                }
-                if(item.getItemId()==R.id.nav_materias){
-                    showSelectedFragment(new MateriaFragment());
-                }
-                return true;
+        mBottomNavigation.setOnNavigationItemSelectedListener(item -> {
+            if(item.getItemId()== R.id.nav_perfil){
+                showSelectedFragment(new PerfilFragment());
             }
+            if(item.getItemId()==R.id.nav_horario){
+                showSelectedFragment(new HorarioFragment());
+            }
+            if(item.getItemId()==R.id.nav_materias){
+                showSelectedFragment(new MateriaFragment());
+            }
+            if(item.getItemId()==R.id.nav_ajustes){
+                showSelectedFragment(new ajustesFragment());
+            }
+            return true;
         });
 
     }
