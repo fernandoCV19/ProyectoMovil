99636d013d54b869b073f80c52b5f5bba158df69
mavl44 <maury.vargasl@gmail.com>
2021-04-28 21:04:35 -0400

    anadiendo constructor, get y set para los atributos

--- Macchiato/app/src/main/java/com/example/macchiato/User.java
+++ Macchiato/app/src/main/java/com/example/macchiato/User.java
@@ -7,4 +7,52 @@ public class User {
     private String uid="";
     private String password="";
 
+    public User(){}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid(){
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return userName;
+    }
+
+
 }
