2ac73d49beb6518d95f0e75b79d863e62afde64f
mavl44 <maury.vargasl@gmail.com>
2021-04-28 21:10:55 -0400

    anadiendo atributos a RegisterActivity

--- Macchiato/app/src/main/java/com/example/macchiato/RegisterActivity.java
+++ Macchiato/app/src/main/java/com/example/macchiato/RegisterActivity.java
@@ -3,8 +3,21 @@ package com.example.macchiato;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText user_R,name_R,email_R,password_R,confirm_R;
+    Button register;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
