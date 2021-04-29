f1f6ca19a5a8e63edf11ca1ea5eadc71bc02b03b
mavl44 <maury.vargasl@gmail.com>
2021-04-28 21:02:36 -0400

    asignando atributos a la clase User

--- Macchiato/app/src/main/java/com/example/macchiato/User.java
+++ Macchiato/app/src/main/java/com/example/macchiato/User.java
@@ -1,4 +1,10 @@
 package com.example.macchiato;
 
 public class User {
+    private String userName="";
+    private String fullName="";
+    private String email="";
+    private String uid="";
+    private String password="";
+
 }
