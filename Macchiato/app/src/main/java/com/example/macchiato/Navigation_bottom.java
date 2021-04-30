dd701d7925fa8d173e493d196301219020a11d9a
mavl44 <maury.vargasl@gmail.com>
2021-04-29 23:20:37 -0400

    mejorando la logica del log in

--- Macchiato/app/src/main/java/com/example/macchiato/Navigation_bottom.java
+++ Macchiato/app/src/main/java/com/example/macchiato/Navigation_bottom.java
@@ -12,25 +12,46 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Navigation_bottom extends AppCompatActivity {
 
+
     BottomNavigationView mBottomNavigation;
+    private FirebaseAuth auth;
+    FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_naivigation_bottom);
         showSelectedFragment(new HorarioFragment());
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth=FirebaseAuth.getInstance();
+
+        firebaseUser = auth.getCurrentUser();
         mBottomNavigation =(BottomNavigationView) findViewById(R.id.bottomNavigationView);
 
         mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if(item.getItemId()== R.id.nav_perfil){
-                    showSelectedFragment(new PerfilFragment());
+                    if(firebaseUser==null){
+                        Toast.makeText(Navigation_bottom.this, "ok", Toast.LENGTH_SHORT).show();
+                        showSelectedFragment(new PerfilFragment());
+                    }else{
+                        Toast.makeText(Navigation_bottom.this, "ya esta loggeado", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
                 if(item.getItemId()==R.id.nav_horario){
                     showSelectedFragment(new HorarioFragment());
@@ -44,6 +65,7 @@ public class Navigation_bottom extends AppCompatActivity {
                 return true;
             }
         });
+
 
     }
 
