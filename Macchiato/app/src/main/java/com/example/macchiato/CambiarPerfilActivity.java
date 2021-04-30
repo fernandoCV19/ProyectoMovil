e9d31678f6f99fee79d7f99832f1e976cbd0958b
mavl44 <maury.vargasl@gmail.com>
2021-04-30 17:15:24 -0400

    validando la nueva contrasena para el usuario

--- Macchiato/app/src/main/java/com/example/macchiato/CambiarPerfilActivity.java
+++ Macchiato/app/src/main/java/com/example/macchiato/CambiarPerfilActivity.java
@@ -1,14 +1,30 @@
 package com.example.macchiato;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CambiarPerfilActivity extends AppCompatActivity {
     EditText contAct,contNueva,contConfir;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String thisUserId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,11 @@ public class CambiarPerfilActivity extends AppCompatActivity {
         contAct = findViewById(R.id.cc_contrActual_id);
         contNueva = findViewById(R.id.cc_contrNueva_id);
         contConfir = findViewById(R.id.cc_repetir_contrNueva_id);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        reference= FirebaseDatabase.getInstance().getReference("User");
+        thisUserId=user.getUid();
+
 
     }
 
@@ -24,10 +45,28 @@ public class CambiarPerfilActivity extends AppCompatActivity {
         String contActText=contAct.getText().toString().trim();
         String contNuevaText=contNueva.getText().toString().trim();
         String contConfirText=contConfir.getText().toString().trim();
+        if(contNuevaText.isEmpty()){
+            contNueva.setError("ingrese su contrasena");
+            contNueva.requestFocus();
+            return;
+        }
+        if(contNuevaText.length()<6){
+            contNueva.setError("la contrasena es muy corta");
+            contNueva.requestFocus();
+            return;
+        }
+        user.updatePassword(contNuevaText).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startActivity(new Intent(CambiarPerfilActivity.this, Navigation_bottom.class));
+            }
 
-        
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(CambiarPerfilActivity.this, "no se pudo cambiar la contrasena", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-
-        startActivity(new Intent(CambiarPerfilActivity.this,Navigation_bottom.class));
     }
 }
