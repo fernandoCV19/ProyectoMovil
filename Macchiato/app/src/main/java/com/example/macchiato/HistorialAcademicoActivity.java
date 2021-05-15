7dde6f662dd0c425ed9f79661c5a6f2d232be8f2
alejandroxp08 <alejandroxp08@gmail.com>
2021-05-14 21:46:55 -0400

    agregado ventana hsitorial academico

---
+++ Macchiato/app/src/main/java/com/example/macchiato/HistorialAcademicoActivity.java
@@ -0,0 +1,14 @@
+package com.example.macchiato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistorialAcademicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_academico) ;
+    }
+}
