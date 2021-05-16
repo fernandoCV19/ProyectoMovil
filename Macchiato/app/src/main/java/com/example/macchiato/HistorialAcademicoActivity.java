ad856ec256827f29a4416119fc00c57b75e013eb
mary173 <53948985+mary173@users.noreply.github.com>
2021-05-16 10:44:23 -0400

    cambios HistorialAcademico

--- Macchiato/app/src/main/java/com/example/macchiato/HistorialAcademicoActivity.java
+++ Macchiato/app/src/main/java/com/example/macchiato/HistorialAcademicoActivity.java
@@ -3,12 +3,20 @@ package com.example.macchiato;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class HistorialAcademicoActivity extends AppCompatActivity {
+   private Spinner sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial_academico) ;
+        sp=(Spinner)findViewById(R.id.materias);
+        String [] opciones={"ingles","algebra","calculo"};
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,opciones);
+        sp.setAdapter(adapter);
     }
+
 }
