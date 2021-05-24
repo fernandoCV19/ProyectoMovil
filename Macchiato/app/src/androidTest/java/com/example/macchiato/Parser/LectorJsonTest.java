3a6a11ef5817c8c1c41f366bcf52d28180338f92
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-05-24 18:51:44 -0400

    Test para la clase lectora de Json creado

--- Macchiato/app/src/androidTest/java/com/example/macchiato/Parser/LectorJsonTest.java
+++ Macchiato/app/src/androidTest/java/com/example/macchiato/Parser/LectorJsonTest.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.firebase.database.ThrowOnExtraProperties;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,6 +30,45 @@ public class LectorJsonTest {
         assertThat(respuesta).isEmpty();
     }
 
-    
+    @Test
+    public void encontrarElArchivoDondeSeEncuentranTodosLosGruposTest(){
+        String respuesta = lectorJson.loadJSONFromAsset("materias.json",context);
+        assertThat(respuesta).isNotEmpty();
+    }
 
+    @Test
+    public void correctaLecturaDelArchivoDondeSeEncuentrarTodosLosGruposTest(){
+        String respuesta = lectorJson.loadJSONFromAsset("materias.json",context);
+        assertThat(respuesta).contains("\"nombreMateria\":\"INGLES I\"");
+        assertThat(respuesta).contains("\"docente\":\"CESPEDES GUIZADA MARIA BENITA\"");
+        assertThat(respuesta).contains("\"codigo\":\"2010010\"");
+    }
+
+    @Test
+    public void encontrarElArchivoDondeSeEncuentranTodasLasMateriasConSusIDsTest(){
+        String respuesta = lectorJson.loadJSONFromAsset("materiasID.json",context);
+        assertThat(respuesta).isNotEmpty();
+    }
+
+    @Test
+    public void correctaLecturaDelArchivoDondeSeEncuentrarTodasLasMateriasConSusIDsTest(){
+        String respuesta = lectorJson.loadJSONFromAsset("materiasID.json",context);
+        assertThat(respuesta).contains("\"nombreMateria\":\"INGLES I\"");
+        assertThat(respuesta).contains("\"requisitos\":[\"Examen de Ingreso\"]");
+        assertThat(respuesta).contains("\"requisitos\":[\"PROGRAMACION FUNCIONAL\", \"PROBABILIDAD Y ESTADISTICA\"]");
+    }
+
+    @Test
+    public void encontrarElArchivoDondeSeEncuentranTodasLasMateriasConSuNivelTest(){
+        String respuesta = lectorJson.loadJSONFromAsset("materiasNivel.json",context);
+        assertThat(respuesta).isNotEmpty();
+    }
+
+    @Test
+    public void correctaLecturaDelArchivoDondeSeEncuentrarTodasLasMateriasConSuNivelTest(){
+        String respuesta = lectorJson.loadJSONFromAsset("materiasNivel.json",context);
+        assertThat(respuesta).contains("\"codigo\":\"1803001\"");
+        assertThat(respuesta).contains("\"nombreMateria\":\" INGLES I\"");
+        assertThat(respuesta).contains("\"nivel\":\"A\"");
+    }
 }
