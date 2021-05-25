e8047d9764a0f1f30484e7a54bdc9c26c7858f0b
alejandroxp08 <alejandroxp08@gmail.com>
2021-05-25 17:08:49 -0400

    modificado horario fragment

--- Macchiato/app/src/main/java/com/example/macchiato/GrupoHorarioAdapter.java
+++ Macchiato/app/src/main/java/com/example/macchiato/GrupoHorarioAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
@@ -12,22 +13,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.macchiato.Models.Clase;
-import com.example.macchiato.Models.Materia;
+import com.example.macchiato.Models.Grupo;
+import com.example.macchiato.Models.Grupo;
+
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MateriaHorarioAdapter extends RecyclerView.Adapter<MateriaHorarioAdapter.ViewHolder>{
-    private List<Materia> mData;
+public class GrupoHorarioAdapter extends RecyclerView.Adapter<GrupoHorarioAdapter.ViewHolder>{
+    private List<Grupo> mData;
     private LayoutInflater mInflater;
     private Context context;
     RecyclerView recyclerView;
     View itemLayoutView;
-    public MateriaHorarioAdapter(List<Materia> mData, Context context) {
+
+    public GrupoHorarioAdapter(List<Grupo> mData, Context context) {
         this.mData = mData;
         this.context = context;
+
     }
 
 
@@ -37,47 +42,39 @@ public class MateriaHorarioAdapter extends RecyclerView.Adapter<MateriaHorarioAd
 
     @NotNull
     @Override
-    public MateriaHorarioAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public GrupoHorarioAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
 
         itemLayoutView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.selected_materia, null);
 
         // create ViewHolder
 
-        MateriaHorarioAdapter.ViewHolder viewHolder = new MateriaHorarioAdapter.ViewHolder(itemLayoutView);
+        GrupoHorarioAdapter.ViewHolder viewHolder = new GrupoHorarioAdapter.ViewHolder(itemLayoutView);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(final MateriaHorarioAdapter.ViewHolder holder, final int position){
+    public void onBindViewHolder(final GrupoHorarioAdapter.ViewHolder holder, final int position){
         holder.bindData(mData.get(position));
 
     }
 
-    public void setItems(List<Materia> items){mData=items;}
+    public void setItems(List<Grupo> items){mData=items;}
 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        Context context;
-        TextView numMateriaInfo,nomDocenteInfo;
-        RecyclerView horariosInfo;
-        CardView color;
-        Button botonDetalles;
+        CheckBox checkBoxGrupo;
 
         public ViewHolder(View itemView) {
             super(itemView);
             context=itemView.getContext();
-
-            this.nomDocenteInfo = itemView.findViewById(R.id.nomDocenteInfo);
-            this.horariosInfo=itemView.findViewById(R.id.horariosInfo);
-            this.botonDetalles=itemView.findViewById(R.id.botonDetalles);
-            this.color= itemView.findViewById(R.id.cardViewMateriaInfo);
+            this.checkBoxGrupo = itemView.findViewById(R.id.checkBoxMateria);
         }
 
-        public void bindData(final Materia item){
-
-
+        public void bindData(final Grupo item){
+            String mostrar=item.getGrupo()+" - "+item.getDocente();
+            checkBoxGrupo.setText(mostrar);
         }
 
     }
--- Macchiato/app/src/main/java/com/example/macchiato/HorarioFragment.java
+++ Macchiato/app/src/main/java/com/example/macchiato/HorarioFragment.java
@@ -4,6 +4,8 @@ package com.example.macchiato;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +14,21 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.macchiato.Models.Grupo;
+import com.example.macchiato.Models.Materia;
+import com.example.macchiato.Servicios.ConsultorMaterias;
+import com.example.macchiato.Servicios.Iniciador;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class HorarioFragment extends Fragment {
     Spinner spinnerNivel;
     Spinner spinnerMateria;
-
+    RecyclerView recyclerView;
+    String selectedClass;
+    String select;
     public HorarioFragment() {
         // Required empty public constructor
     }
@@ -25,16 +37,23 @@ public class HorarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_horario, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerCheckbox);
         spinnerNivel = view.findViewById(R.id.spinnerNivel);
         spinnerMateria = view.findViewById(R.id.spinnerMateria);
-
+        Iniciador iniciador=new Iniciador();
+        try {
+            iniciador.iniciar(getContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         spinnerNivel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
 
-                String selectedClass = parent.getItemAtPosition(position).toString();
+                selectedClass = parent.getItemAtPosition(position).toString();
                 switch (selectedClass) {
                     case "A":
 
@@ -95,8 +114,8 @@ public class HorarioFragment extends Fragment {
         spinnerMateria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-
+                select = spinnerMateria.getSelectedItem().toString();
+                cambiarRecycler();
             }
 
             @Override
@@ -106,11 +125,33 @@ public class HorarioFragment extends Fragment {
 
         });
 
+        ArrayList<Materia> materias = ConsultorMaterias.getMaterias();
+        ArrayList<Grupo>   grupos = new ArrayList<>();
+        for(Materia materia: materias){
+            if(materia.getNombre().equals(select)){
+                grupos = materia.getGrupos();
+            }
+        }
+        GrupoHorarioAdapter materiaHorarioAdapter= new GrupoHorarioAdapter(grupos,this.getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(materiaHorarioAdapter);
 
 
         return view;
-
-
+    }
+    void cambiarRecycler(){
+        ArrayList<Materia> materias = ConsultorMaterias.getMaterias();
+        ArrayList<Grupo>   grupos = new ArrayList<>();
+        for(Materia materia: materias){
+            if(materia.getNombre().equals(select)){
+                grupos = materia.getGrupos();
+            }
+        }
+        GrupoHorarioAdapter materiaHorarioAdapter= new GrupoHorarioAdapter(grupos,this.getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(materiaHorarioAdapter);
     }
 
 }
