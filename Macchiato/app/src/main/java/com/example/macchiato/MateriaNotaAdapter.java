49adc57a3c9af5b62b14b503b1c1adb74fdd87a2
mary173 <53948985+mary173@users.noreply.github.com>
2021-05-29 22:46:48 -0400

    checkbox historial

--- Macchiato/app/src/main/java/com/example/macchiato/MateriaNotaAdapter.java
+++ Macchiato/app/src/main/java/com/example/macchiato/MateriaNotaAdapter.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
@@ -17,7 +19,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class MateriaNotaAdapter extends RecyclerView.Adapter<MateriaNotaAdapter.ViewHolder>{
+public class MateriaNotaAdapter extends RecyclerView.Adapter<MateriaNotaAdapter.ViewHolder> {
     private List<MateriaNota> mData;
     private LayoutInflater mInflater;
     private Context context;
@@ -28,11 +30,13 @@ public class MateriaNotaAdapter extends RecyclerView.Adapter<MateriaNotaAdapter.
     }
 
     @Override
-    public int getItemCount(){ return mData.size();}
+    public int getItemCount() {
+        return mData.size();
+    }
 
     @NotNull
     @Override
-    public MateriaNotaAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public MateriaNotaAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View itemLayoutView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.materias_element_historial, null);
@@ -44,18 +48,27 @@ public class MateriaNotaAdapter extends RecyclerView.Adapter<MateriaNotaAdapter.
     }
 
     @Override
-    public void onBindViewHolder(final MateriaNotaAdapter.ViewHolder holder, final int position){
+    public void onBindViewHolder(final MateriaNotaAdapter.ViewHolder holder, final int position) {
 
         holder.bindData(mData.get(position));
+        holder.checkBox.setChecked(mData.get(position).esSeleccionado());
+
+        holder.setItemClickListener(new ViewHolder.);
     }
 
-    public void setItems(List<MateriaNota> items){mData=items;}
+
+    public void setItems(List<MateriaNota> items) {
+        mData = items;
+    }
 
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+
+    static  class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         TextView nomMateria,notaMateria;
         CardView color;
+        CheckBox checkBox;
+        ItemClickListener itemClickListener;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -63,7 +76,11 @@ public class MateriaNotaAdapter extends RecyclerView.Adapter<MateriaNotaAdapter.
             this.nomMateria = itemView.findViewById(R.id.nomMateria);
             this.notaMateria = itemView.findViewById(R.id.nota);
             this.color= itemView.findViewById(R.id.cardViewMateria);
+            this.itemClickListener=itemView.findViewById(R.id.eliminar);
+            checkBox.setOnClickListener(this);
         }
+
+
 
         public void bindData(final MateriaNota item){
             nomMateria.setText(item. getMateria());
@@ -71,5 +88,23 @@ public class MateriaNotaAdapter extends RecyclerView.Adapter<MateriaNotaAdapter.
             notaMateria.setText(String.valueOf(num));
           // color.setCardBackgroundColor(Color.parseColor(item.getColor()));
         }
+        public void setItemClickListener(ItemClickListener ic){
+         this.itemClickListener=ic;
+        }
+
+        public ItemClickListener ItemClickListener() {
+            return itemClickListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            this.itemClickListener.OnItemClick(v,getLayoutPosition());
+
+        }
+    }
+    interface ItemClickListener{
+        void OnItemClick(View v,int pos);
+
+
     }
 }
