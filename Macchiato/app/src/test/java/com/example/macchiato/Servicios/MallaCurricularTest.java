b1b3aaf7fb4e0410fdade7d80f22756ed1fb7aad
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-06-01 17:03:11 -0400

    Test para Malla Curricular terminados (pendiente a correccion de falla)

--- Macchiato/app/src/test/java/com/example/macchiato/Servicios/MallaCurricularTest.java
+++ Macchiato/app/src/test/java/com/example/macchiato/Servicios/MallaCurricularTest.java
@@ -23,7 +23,7 @@ public class MallaCurricularTest {
     }
 
     @Test
-    public void cuandoGetSiguientesConUnEstudianteNuevoEntoncesDevuelveUnaListaConLasMateriasQueDeberiamosTomar(){
+    public void dadoUnEstudianteNuevocuandoGetSiguientesEntoncesDevuelveUnaListaConLasMateriasQueDeberiamosTomar(){
         ArrayList<Integer> ids = mallaCurricular.getSiguientes();
         assertThat(ids).hasSize(5);
         assertThat(ids).contains(1);
@@ -34,13 +34,13 @@ public class MallaCurricularTest {
     }
 
     @Test
-    public void cuandoGetSinTomarConUnEstudianteNuevoEntoncesDevuelveUnaListaConTodasLasListasDeLaCarrera(){
+    public void dadoUnEstudianteNuevoCuandoGetSinTomarEntoncesDevuelveUnaListaConTodasLasListasDeLaCarrera(){
         ArrayList<Integer> ids = mallaCurricular.getSinTomar();
         assertThat(ids).hasSize(41);
     }
 
     @Test
-    public void cuandoUnEstudianteAproboTodoElPrimerSemestreCuandoQuitarEntoncesAlLlamarGetSiguientesEntoncesDevuelveLasMateriasDelSegundoSemestre(){
+    public void dadoUnEstudianteAproboTodoElPrimerSemestreCuandoQuitarEntoncesAlLlamarGetSiguientesEntoncesDevuelveLasMateriasDelSegundoSemestre(){
         ArrayList<Integer> aprobadas = new ArrayList<>();
         llenarIds(aprobadas, 1, 5);
         aprobadas.add(1);aprobadas.add(2);aprobadas.add(3);aprobadas.add(4);aprobadas.add(5);
@@ -57,7 +57,7 @@ public class MallaCurricularTest {
     }
 
     @Test
-    public void cuandoUnEstudianteAproboTodoElPrimerSemestreCuandoQuitarEntoncesAlLlamarGetSinTomarEntoncesDevuelveLasMateriasQueFaltaPorTomar(){
+    public void dadoUnEstudianteAproboTodoElPrimerSemestreCuandoQuitarEntoncesAlLlamarGetSinTomarEntoncesDevuelveLasMateriasQueFaltaPorTomar(){
         ArrayList<Integer> aprobadas = new ArrayList<>();
         llenarIds(aprobadas, 1, 5);
         mallaCurricular.quitar(aprobadas);
@@ -67,7 +67,7 @@ public class MallaCurricularTest {
     }
 
     @Test
-    public void cuandoUnEstudianteAproboTodoElSegundoSemestreCuandoGetSiguientesEntoncesDevuelveLasMateriasDelTercerSemestre(){
+    public void dadoUnEstudianteAproboTodoElSegundoSemestreCuandoGetSiguientesEntoncesDevuelveLasMateriasDelTercerSemestre(){
         ArrayList<Integer> aprobadas = new ArrayList<>();
         llenarIds(aprobadas, 1, 11);
         mallaCurricular.quitarVarios(aprobadas);
@@ -84,7 +84,7 @@ public class MallaCurricularTest {
     }
 
     @Test
-    public void cuandoUnEstudianteAproboTodoSegundoSemestreCuandoQuitarEntoncesAlLlamarGetSinTomarEntoncesDevuelveLasMateriasQueFaltaPorTomar(){
+    public void dadoUnEstudianteAproboTodoSegundoSemestreCuandoQuitarEntoncesAlLlamarGetSinTomarEntoncesDevuelveLasMateriasQueFaltaPorTomar(){
         ArrayList<Integer> aprobadas = new ArrayList<>();
         llenarIds(aprobadas, 1, 11);
         mallaCurricular.quitarVarios(aprobadas);
@@ -94,7 +94,7 @@ public class MallaCurricularTest {
     }
 
     @Test
-    public void cuandoUnEstudianteAproboHastaQuintoSemestreCuandoGetSiguientesEntoncesDevuelveLasMateriasDelSextoSemestre(){
+    public void dadoUnEstudianteAproboHastaQuintoSemestreCuandoGetSiguientesEntoncesDevuelveLasMateriasDelSextoSemestre(){
         ArrayList<Integer> aprobadas = new ArrayList<>();
         llenarIds(aprobadas, 1, 29);
 
@@ -109,6 +109,110 @@ public class MallaCurricularTest {
         assertThat(ids).contains(34);
         assertThat(ids).contains(35);
 
+    }
+
+    @Test
+    public void dadoUnEstudianteApruebaHastaQuintoSemestreCuandoGetSinTomarEntoncesDevuelveUnaListaDeLasMateriasSinTomar(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        llenarIds(aprobadas, 1, 29);
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSinTomar();
+
+        assertThat(ids).hasSize(12);
+    }
+
+    @Test
+    public void dadoUnEstudianteQueAproboTodasLasMateriasCuandoGetSinTomarEntoncesDevulveProyectoDeGrado2(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        llenarIds(aprobadas, 1, 35);
+        aprobadas.add(37);aprobadas.add(38);
+        aprobadas.add(40);aprobadas.add(41);
+        aprobadas.add(44);
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSinTomar();
+
+        assertThat(ids).hasSize(1);
+        assertThat(ids.get(0)).isEqualTo(53);
+    }
+
+    //Falla
+    @Test
+    public void dadoUnEstudianteQueAproboTodasLasMateriasCuandoGetSiguientesEntoncesDevuelveProyectoDeGrado2(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        llenarIds(aprobadas, 1, 32);
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSiguientes();
+
+        assertThat(ids).hasSize(4);
+    }
+
+    @Test
+    public void dadoUnEstudianteConAprobacionesEnDiferentesSemestresCuandoGetSinTomarEntoncesDevuelveLaMateriaQueDeberiaTomar(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        aprobadas.add(1);aprobadas.add(6); //16
+        aprobadas.add(3); //7
+        aprobadas.add(4);aprobadas.add(8);aprobadas.add(12);//18
+        aprobadas.add(5);//9 y 11
+        aprobadas.add(2);aprobadas.add(10);//14
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSiguientes();
+
+        assertThat(ids).hasSize(6);
+        assertThat(ids).contains(16);
+        assertThat(ids).contains(7);
+        assertThat(ids).contains(18);
+        assertThat(ids).contains(9);
+        assertThat(ids).contains(11);
+        assertThat(ids).contains(14);
+    }
+
+    @Test
+    public void dadoUnAlumnoConMateriasPendientesCuandoGetSiguientesEntonesDevuelveLasMateriasFaltantes(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        llenarIds(aprobadas,6,11);
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSiguientes();
+        assertThat(ids).hasSize(5);
+        assertThat(ids).contains(1);
+        assertThat(ids).contains(2);
+        assertThat(ids).contains(3);
+        assertThat(ids).contains(4);
+        assertThat(ids).contains(5);
+    }
+
+    @Test
+    public void dadoUnAlumnoQueAproboProgramacionFuncionalYProbabilidadYEstadisticaCuandoGetSiguientesEntoncesDevuelveIAI(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        aprobadas.add(3);aprobadas.add(7);aprobadas.add(13);aprobadas.add(22);
+        aprobadas.add(4);aprobadas.add(8);aprobadas.add(12);aprobadas.add(18);
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSiguientes();
+        assertThat(ids).hasSize(4);
+        assertThat(ids).contains(29);
+    }
+
+    @Test
+    public void dadoUnAlumnoQueAproboTallerDeProgramacionCuandoGetSiguientesEntoncesDevuelveSistemasOperativosYAutomatas(){
+        ArrayList<Integer> aprobadas = new ArrayList<>();
+        aprobadas.add(2);aprobadas.add(10);aprobadas.add(14);aprobadas.add(19);
+
+        mallaCurricular.quitarVarios(aprobadas);
+
+        ArrayList<Integer> ids = mallaCurricular.getSiguientes();
+        assertThat(ids).contains(25);
+        assertThat(ids).contains(27);
     }
 
     private void llenarIds(ArrayList<Integer> ids, int inicio, int fin){
