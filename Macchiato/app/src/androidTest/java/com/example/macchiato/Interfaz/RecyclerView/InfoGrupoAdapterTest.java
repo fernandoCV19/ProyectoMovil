d5fdd58b7ffb6a6a2564c614dde0d9aa0b545ce5
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-06-10 21:46:24 -0400

    Aumentando test

--- Macchiato/app/src/androidTest/java/com/example/macchiato/Interfaz/RecyclerView/InfoGrupoAdapterTest.java
+++ Macchiato/app/src/androidTest/java/com/example/macchiato/Interfaz/RecyclerView/InfoGrupoAdapterTest.java
@@ -1,6 +1,27 @@
 package com.example.macchiato.Interfaz.RecyclerView;
 
-import static org.junit.Assert.*;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import org.junit.After;
+import org.junit.runner.RunWith;
+
+import com.example.macchiato.Navigation_bottom;
+import com.example.macchiato.R;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.internal.configuration.injection.filter.NameBasedCandidateFilter;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.allOf;
 
 public class InfoGrupoAdapterTest {
 
