9bad188dec9b7afbe78d7dcf2ec854afd1ce5a99
fernandoCV19 <64548505+fernandoCV19@users.noreply.github.com>
2021-06-11 20:03:06 -0400

    Test para navegacion en inicio de sesion

---
+++ Macchiato/app/src/androidTest/java/com/example/macchiato/Interfaz/Navegacion/NavegacionEnLaVentanaDeInicioDeSesion.java
@@ -0,0 +1,59 @@
+package com.example.macchiato.Interfaz.Navegacion;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import com.example.macchiato.Navigation_bottom;
+import com.example.macchiato.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class NavegacionEnLaVentanaDeInicioDeSesion {
+
+    @Rule
+    public ActivityScenarioRule activityScenarioRule = new ActivityScenarioRule(Navigation_bottom.class);
+
+    @Test
+    public void deVentanaSinInicarSesionAIniciarSesion() {
+        onView(withId(R.id.nav_perfil)).perform(click());
+        onView(withId(R.id.buttonIniciarSesion)).perform(click());
+        onView(withId(R.id.logInActivity)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void deVentanaIniciarSesionRetrocederASinIniciarSesion(){
+        onView(withId(R.id.nav_perfil)).perform(click());
+        onView(withId(R.id.buttonIniciarSesion)).perform(click());
+        onView(withId(R.id.logInActivity)).check(matches(isDisplayed()));
+        onView(isRoot()).perform(ViewActions.pressBack());
+        onView(withId(R.id.perfilSinIniciarFragment)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void deVentanaDeInicioDeSesionARegistrarse(){
+        onView(withId(R.id.nav_perfil)).perform(click());
+        onView(withId(R.id.buttonIniciarSesion)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.registerActivity)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void deRegistrarseAVentnaDeInicioDeSesion(){
+        onView(withId(R.id.nav_perfil)).perform(click());
+        onView(withId(R.id.buttonIniciarSesion)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+        onView(withId(R.id.registerActivity)).check(matches(isDisplayed()));
+        onView(isRoot()).perform(ViewActions.pressBack());
+        onView(withId(R.id.logInActivity)).check(matches(isDisplayed()));
+    }
+}
